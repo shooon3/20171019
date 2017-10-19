b4c2533dc52a9e966faa5ba0c929fa014531c5b6
カキハナ_NKC <nkc.taisei.kakihana2016@gmail.com>
2017-10-19 14:55:07 +0900

    プレイヤー基本ステータスクラスとプレイヤークラスを作成

---
+++ Assets/CharactorStatus.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharactorStatus : MonoBehaviour
+{
+    // キャラクターの基本ステータスのデータを格納しているクラス
+
+    // 市民（プレイヤー）の基本ステータス
+    private int playerHp = 100; // HP
+    const int playerMaxHp = 100; // 最大HP
+    private int playerAtk = 1; // 攻撃力
+    private float playerSpeed = 1.0f; // 移動速度
+
+    // テロリスト（プレイヤー）の基本ステータス
+    private int terroristHp = 100;
+    const int terroristMaxHp = 100;
+    private int terroristAtk = 10;
+    private float terroristSpeed = 1.5f;
+
+    // 市民（NPC）の基本ステータス
+    private int citizenHp = 100;
+    const int citizenMaxHp = 100;
+    private int citizenAtk = 1;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    /*市民（プレイヤー）データ送信メソッド*/
+    public int postplayerHp() // HPを送る
+    {
+        return playerHp;
+    }
+    public int postplayerMaxHp() // 最大HPを送る
+    {
+        return playerMaxHp;
+    }
+    public int postplayerAtk() // 攻撃力を送る
+    {
+        return playerAtk;
+    }
+    public float postplayerSpeed() // 移動速度を送る
+    {
+        return playerSpeed;
+    }
+
+    /*テロリスト（プレイヤー）データ送信メソッド*/
+    public int postterroristHp()
+    {
+        return terroristHp;
+    }
+    public int postterroristMaxHp()
+    {
+        return terroristMaxHp;
+    }
+    public int postterroristAtk()
+    {
+        return terroristAtk;
+    }
+    public float postterroristSpeed()
+    {
+        return terroristSpeed;
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    // プレイヤークラス
+    [SerializeField]
+    private int playerGroup = 0; // プレイヤーの属性 0で市民（NPC）1で市民（プレイヤー）2でテロリスト（プレイヤー）
+
+    const string statusName = "Status"; // ステータスオブジェクトの名前
+    private GameObject statusObj; // ステータスオブジェクトを格納する変数
+
+    CharactorStatus charactorstatus; // 各種キャラクターデータを参照するクラス
+
+   public int charactorHp = 0; // 自キャラのHP
+   public int charactorMaxHp = 0; // 自キャラの最大HP
+   public int charactorAtk = 0; // 自キャラの攻撃力
+   public float charactorSpeed = 0.0f; // 自キャラの移動速度
+
+	// Use this for initialization
+	void Start () {
+
+        // ステータスオブジェクトの名前を参照し格納、CharactorStatusコンポーネントを取得
+        statusObj = GameObject.Find(statusName);
+        charactorstatus = statusObj.GetComponent<CharactorStatus>();
+
+        // playerGroupの数値によって各種キャラクターのデータを取得
+        switch (playerGroup)
+        {
+            case 0:
+                setcitizenStatus(); // 市民（NPC）用データ取得メソッドを起動
+                break;
+            case 1:
+                setplayerStatus(); // 市民（プレイヤー）用データ取得メソッドを起動
+                break;
+            case 2:
+                setterroristStatus(); // テロリスト（プレイヤー）用データ取得メソッドを起動
+                break;
+        }
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void setcitizenStatus() // 市民（NPC）データ取得メソッド、各種データを取得し変数に格納
+    {
+        
+    }
+    void setplayerStatus() // 市民（プレイヤー）用データ取得メソッド
+    {
+        charactorHp = charactorstatus.postplayerHp();
+        charactorMaxHp = charactorstatus.postplayerMaxHp();
+        charactorAtk = charactorstatus.postplayerAtk();
+        charactorSpeed = charactorstatus.postplayerSpeed();
+    }
+    void setterroristStatus() // テロリスト（プレイヤー）用データ取得メソッド
+    {
+        charactorHp = charactorstatus.postterroristHp();
+        charactorMaxHp = charactorstatus.postterroristMaxHp();
+        charactorAtk = charactorstatus.postterroristAtk();
+        charactorSpeed = charactorstatus.postterroristSpeed();
+    }
+}
