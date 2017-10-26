ff83bd2a11bf35c8f0c2623cef64f3a7bd770ac8
Sho-n3 <nkc.shota.tanikawa@gmail.com>
2017-10-26 14:59:21 +0900

    photon ルーム作成途中

---
+++ Assets/PhotonManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhotonManager : Photon.MonoBehaviour {
+
+    void ConnectPhoton()
+    {
+        PhotonNetwork.ConnectUsingSettings("v1.0");
+    }
+
+    void OnjoinedLobby()
+    {
+        Debug.Log("PhotonManeger OnJoinedLobby");
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
